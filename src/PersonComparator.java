b5af1bdaac7fad4fb42eca9d2d1a5dd7a232c254
Inna Patsyuk <ig.patsyuk@yandex.ru>
2022-08-22 09:40:13 +0300

    add class Comparator

---
+++ src/PersonComparator.java
@@ -0,0 +1,12 @@
+import java.util.Comparator;
+
+public class PersonComparator implements Comparator<Person> {
+
+    @Override
+    public int compare(Person p1, Person p2) {
+        String[] p1Surname = p1.getSurname().split(" ");
+        String[] p2Surname = p2.getSurname().split(" ");
+        int compareLength = Integer.compare(p1Surname.length, p2Surname.length);
+        return compareLength == 0 ? compareLength : Integer.compare(p1.getAge(), p2.getAge());
+    }
+}
