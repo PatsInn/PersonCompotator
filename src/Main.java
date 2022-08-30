357d4b050aa2b4121243044065f771e3f7740e17
Inna Patsyuk <ig.patsyuk@yandex.ru>
2022-08-30 15:52:59 +0300

    added Predicate

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> personList = new ArrayList<>();
-        int maxLengthSurname = 2;
+        int maxLengthSurname = 3;
         personList.add(new Person("Иван", "Иванов Петров", 29));
         personList.add(new Person("Любовь", "Щелканова Воронина при", 12));
-        personList.add(new Person("Мария", "Трифонова", 36));
+        personList.add(new Person("Мария", "Трифонова", 15));
         personList.add(new Person("Марат", "Марков Мерков", 25));
         personList.add(new Person("Юлия", "Сидорова Карпунина", 42));
-        personList.add(new Person("Сергей", "Муркин Туркин Делов Меркин", 29));
+        personList.add(new Person("Сергей", "Муркин Туркин Делов Меркин", 13));
         System.out.println("Список до сортировки:");
         System.out.println(personList);
 
@@ -24,10 +25,17 @@ public class Main {
             int compareLength = Integer.compare(p1LengthSurname, p2LengthSurname);
             return compareLength == 0 ? Integer.compare(p1.getAge(), p2.getAge()) : compareLength;
         };
-        
+
+
         Collections.sort(personList, comparator);
         System.out.println("Список после сортировки:");
         System.out.println(personList);
+
+        Predicate<Person> personPredicate = person -> person.getAge() < 18;
+        personList.removeIf(personPredicate);
+        System.out.println("Список после удаления:");
+        System.out.println(personList);
+
     }
 
 
